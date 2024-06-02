8ea89792d691fbd8a5976db6502da269500ebbad
emiz <emiz@slac.stanford.edu>
2024-06-02 10:28:59 -0700

    experiment with pub cv

---
+++ cv/cv.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import awkward as ak
+from pathlib import Path
+
+
+def print_items(df: pd.DataFrame):
+    for i in range(len(df)):
+        start_date_str = df.iloc[i]["Start Date"].strftime("%b %Y")
+        end_date_str = df.iloc[i]["End Date"].strftime("%b %Y")
+
+        if not pd.isnull(df.iloc[i]["Description"]):
+            desc_str = f"{df.iloc[i]['Description']}"
+        else:
+            desc_str = ""
+
+        desc_str = f"{df.iloc[i]['Item']}, *{desc_str}*"
+
+        if start_date_str == end_date_str:
+            date_str = f"_{start_date_str}_"
+        else:
+            date_str = f"_{start_date_str} - {end_date_str}_"
+
+        if not pd.isnull(df.iloc[i]["Information"]):
+            info_str = f"#grid(columns: (3%, 97%), align(left)[], align(left)[{df.iloc[i]['Information']}])"
+        else:
+            info_str = ""
+
+        print(
+            f"#grid(columns: (75%, 25%), align(left)[{desc_str}], align(right)[{date_str}]) {info_str}"
+        )
+
+
+cv_data = ak.from_json(Path("/home/emiz/projects/e-miz.github.io/cv/pubs.json"))
+
+pubs_df = pd.read_csv("/home/emiz/projects/e-miz.github.io/cv/pubs.csv")
+
+# need to drop NaN before matchins
+mypubs_df = pubs_df[pubs_df["Manual Tags"].str.match("mypublication")]
